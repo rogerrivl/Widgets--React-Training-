5ef8fb68353ebe6b8190c1888d5caaa61995e501
Roger <rogerrivl.dev@gmail.com>
2022-06-07 23:16:11 +0200

    Widgets

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers front",
+  },
+  {
+    title: "How do you use React?",
+    content: "React is used by creating components",
+  },
+];
+
+export default () => {
+  return (
+    <div>
+      {/* <Accordion items={items} /> */}
+      <Search />
+    </div>
+  );
+};
